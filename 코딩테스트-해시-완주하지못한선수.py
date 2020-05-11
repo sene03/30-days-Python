05b6ce01917952a3cba02320b93cb7abb518c89a
sene03 <65144472+sene03@users.noreply.github.com>
2020-05-11 13:23:20 +0900

    Answer?
    정답코드 효율성 부족함. 개선?
    
    문제
    리스트 element로 돌면서 지우면 안되는 이유: 인덱스를 기준으로 하기 때문에 for문 한번 돌아서 첫번째 요소가 지워지면 두번째(새로운 리스트의 첫번째 요소)로 for을 도는 게 아니라 세번째(새로운 리스트의 두번째 요소)를 가지고 돌게 된다... ㅇㅋ?
    
    해결
    인덱스로 돌아야!

--- 코딩테스트-해시-완주하지못한선수.py
+++ 코딩테스트-해시-완주하지못한선수.py
@@ -27,3 +27,14 @@ solution(p2, c2)
 #solution(p3, c3)
 
 #answer leo / vinko / mislav
+
+#answer code?
+def solution(participant, completion):
+    for i in range(len(participant)):
+        if participant[i] in completion:
+            participant[i] == "O"
+            completion.remove(participant[i])
+        else:
+            return participant[i]
+
+
