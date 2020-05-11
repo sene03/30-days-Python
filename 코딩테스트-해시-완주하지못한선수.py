d9360019c862347d19e977a8ae5164bd5c03dd50
sene03 <65144472+sene03@users.noreply.github.com>
2020-05-11 13:17:05 +0900

    Add files via upload

---
+++ 코딩테스트-해시-완주하지못한선수.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 12:36:35 2020
+
+@author: hanse
+"""
+
+
+def solution(participant, completion):
+    for i in participant:
+        if i in completion:
+            print(i)
+            participant.remove(i)
+            completion.remove(i)
+            print(participant, completion)
+        else:
+            return print(i)
+        
+p1 = ['leo', 'kiki', 'eden']
+c1 = ['eden', 'kiki']
+p2 = ['marina', 'josipa', 'nikola', 'vinko', 'filipa'] 
+c2 = ['josipa', 'filipa', 'marina', 'nikola']
+p3 = ['mislav', 'stanko', 'mislav', 'ana']; c3 = ['stanko', 'ana', 'mislav']
+
+#solution(p1, c1)
+solution(p2, c2)
+#solution(p3, c3)
+
+#answer leo / vinko / mislav
